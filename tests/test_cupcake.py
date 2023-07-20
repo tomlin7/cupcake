ed392b1b9032c24bc5650a54e671ff34d508d92f
Billy <billydevbusiness@gmail.com>
2023-07-21 04:51:43 +0800

    test: Make tests pass

--- tests/test_cupcake.py
+++ tests/test_cupcake.py
@@ -1,5 +1,10 @@
+import sys
+from os.path import abspath, dirname, join
+sys.path.append(abspath(join(dirname(__file__), '..')))
+
+
 from cupcake import __version__
 
 
 def test_version():
-    assert __version__ == '0.10.0'
+    assert __version__ == '0.15.0'
