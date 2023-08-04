6390c304e39f160a903f623d9403d85398babf8b
Billy <billydevbusiness@gmail.com>
2023-08-05 01:35:30 +0800

    chore: Bump version to `0.25.7`

--- cupcake/__init__.py
+++ cupcake/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.25.7'
+__version__ = '0.25.8'
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
 
 __all__ = ["Editor", "get_editor", "DiffEditor", "ImageViewer", "TextEditor", "Config", "Languages"]
